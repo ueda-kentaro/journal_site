13933d566d69962cd9684a06a10567178b622faa
ueda kentaro <nowallcloud9+ueda@gmail.com>
2016-12-23 14:18:49 +0000

    投稿機能付きジャーナルサイトの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
   get '/articles' => 'articles#index'
-  
+  get '/articles/new' => 'articles#new'
+  post '/articles' => 'articles#create'
   get '/articles/:id' => 'articles#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
